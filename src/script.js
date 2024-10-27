59dc379965664376da96dd05ddc5ee7d70b5033d
Joseleelsuper <tory002254@gmail.com>
2024-10-27 13:20:48 +0100

    Creado portfolio completo

--- src/script.js
+++ src/script.js
@@ -29,27 +29,69 @@ document.addEventListener('DOMContentLoaded', function() {
     const projects = [
         {
             title: "TNGLore",
-            description:
-            `
-            Es un proyecto en conjunto que abarca una página web, un bot de Discord y una base de datos MongoDB.
-            Lo empezamos en verano, y tiene el objetivo de conseguir cartas del lore (historia, momentos de recuerdo) de un grupo de amigos. Estas cartas se podrán conseguir dentro de cofres, los cuales los obtienes de forma aleatoria hablando por un canal de texto en discord. El bot tiene una probabilidad de responder al mensaje enviado y ofrecer un cofre al usuario con una carta aleatoria. Cada una de las cartas tiene su propia descripción, colección y rareza. La página web sirve para registrarse en el proyecto y poder acceder a tus cartas, las colecciones y un marketplace, cuya finalidad es poder intercambiar cartas entre usuarios.
-
-            La página web está siendo desarrollada por Javier Troyano con Express.js y el bot de discord por mí, José Gallardo, con Python.    
-            `,
+            description: {
+                es: [
+                    "Es un proyecto en conjunto que abarca una página web, un bot de Discord y una base de datos MongoDB.",
+                    "Lo empezamos en verano, y tiene el objetivo de conseguir cartas del lore (historia, momentos de recuerdo) de un grupo de amigos.",
+                    "Estas cartas se podrán conseguir dentro de cofres, los cuales los obtienes de forma aleatoria hablando por un canal de texto en discord.",
+                    "El bot tiene una probabilidad de responder al mensaje enviado y ofrecer un cofre al usuario con una carta aleatoria.",
+                    "Cada una de las cartas tiene su propia descripción, colección y rareza.",
+                    "La página web sirve para registrarse en el proyecto y poder acceder a tus cartas, las colecciones y un marketplace, cuya finalidad es poder intercambiar cartas entre usuarios.",
+                    "La página web está siendo desarrollada por Javier Troyano con Express.js y el bot de discord por mí, José Gallardo, con Python."
+                ],
+                en: [
+                    "This is a collaborative project that includes a website, a Discord bot, and a MongoDB database.",
+                    "We started it in the summer with the goal of collecting lore cards (history, memorable moments) of a group of friends.",
+                    "These cards can be obtained from chests, which are randomly acquired by chatting in a Discord text channel.",
+                    "The bot has a chance to respond to the sent message and offer a chest to the user with a random card.",
+                    "Each card has its own description, collection, and rarity.",
+                    "The website is used to register for the project and access your cards, collections, and a marketplace, which allows users to exchange cards.",
+                    "The website is being developed by Javier Troyano using Express.js, and I, José Gallardo, am developing the Discord bot using Python."
+                ]
+            },
             image: "../assets/proyects/TNGLore.webp",
             web: "https://tnglore.onrender.com/"
         },
         {
-            title: "Proyecto 2",
-            description: "Descripción del proyecto 2",
-            image: "https://via.placeholder.com/500x300",
-            github: "https://github.com/usuario/proyecto2"
+            title: "JoseleelBot",
+            description: {
+                es: [
+                    "Es un bot de discord multitarea y de moderación. Algunas de sus funcionalidades son:",
+                    "- Reemplazar las URLs de redes sociales pasadas en canales de texto por URLs que arreglan los embeds en Discord.",
+                    "- Avisa a los amigos de las personas cuyo día es su cumpleaños.",
+                    "- Colabora con el proyecto de TNGLore para otorgar cofres a los usuarios que hablen por canales de texto.",
+                    "- Utiliza la API de Groq (IA) para que, con un comando, puedas ser respondido con inteligencia artificial.",
+                    "- Soporta ficheros de texto, imágenes y audios."
+                ],
+                en: [
+                    "This is a multitasking and moderation Discord bot. Some of its features include:",
+                    "- Replacing social media URLs posted in text channels with URLs that fix embeds in Discord.",
+                    "- Notifying friends when it's someone's birthday.",
+                    "- Collaborating with the TNGLore project to award chests to users who chat in text channels.",
+                    "- Using the Groq API (AI) to respond to commands with artificial intelligence.",
+                    "- Supporting text files, images, and audio."
+                ]
+            },
+            image: "../assets/proyects/JoseleelBot.webp",
         },
         {
-            title: "Proyecto 3",
-            description: "Descripción del proyecto 3",
-            image: "https://via.placeholder.com/500x300",
-            web: "https://proyecto3.com"
+            title: "Talf",
+            description: {
+                es: [
+                    "Talf es el nombre del proyecto que hice durante la asignatura de Metodología de la programación (Mepro).",
+                    "Se trata de recrear dos juegos de mesa 'vikingos' llamados Brandubh y Ardri, los cuales elijes uno de los dos al iniciar el programa.",
+                    "Debía de ser desarrollado en Java-20 y comprobado su funcionamiento con test de JUnit.",
+                    "Ha sido mi primer proyecto profesional de programación en el que se juntaron varias tareas, como mantener limpieza del código, manejo de excepciones y, sobretodo, el aprendizaje que conllevó hacer el juego."
+                ],
+                en: [
+                    "Talf is the name of the project I did during the Programming Methodology (Mepro) course.",
+                    "It involves recreating two 'Viking' board games called Brandubh and Ardri, one of which you choose when starting the program.",
+                    "It had to be developed in Java-20 and its functionality verified with JUnit tests.",
+                    "This was my first professional programming project that combined several tasks, such as maintaining code cleanliness, exception handling, and, above all, the learning experience that came with creating the game."
+                ]
+            },
+            image: "../assets/proyects/Talf.webp",
+            github: "https://github.com/Joseleelsuper/Ardri"
         }
     ];
 
@@ -58,21 +100,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const nextButton = document.querySelector('#proyectos .carousel-button.next');
     let currentIndex = 0;
 
-    function createProjectCard(project, index) {
+    function createProjectCard(project, index, language) {
         const card = document.createElement('div');
         card.className = 'project-card';
         if (index === currentIndex) {
             card.classList.add('active');
-        } else if (index === (currentIndex - 1 + projects.length) % projects.length) {
-            card.classList.add('prev');
-        } else if (index === (currentIndex + 1) % projects.length) {
-            card.classList.add('next');
         }
+        const descriptionHtml = project.description[language].map(paragraph => `<p>${paragraph}</p>`).join('');
         card.innerHTML = `
             <img src="${project.image}" alt="${project.title}" class="project-image">
             <div class="project-info">
                 <h3 class="project-title">${project.title}</h3>
-                <p class="project-description">${project.description}</p>
+                <div class="project-description">${descriptionHtml}</div>
                 <div class="project-links">
                     ${project.github ? `<a href="${project.github}" class="project-link" target="_blank" rel="noopener noreferrer"><span class="material-icons">code</span></a>` : ''}
                     ${project.web ? `<a href="${project.web}" class="project-link" target="_blank" rel="noopener noreferrer"><span class="material-icons">language</span></a>` : ''}
@@ -82,67 +121,33 @@ document.addEventListener('DOMContentLoaded', function() {
         return card;
     }
 
-    function updateCarousel() {
+    function updateProjectCarousel(language) {
         carousel.innerHTML = '';
         projects.forEach((project, index) => {
-            const card = createProjectCard(project, index);
+            const card = createProjectCard(project, index, language);
             carousel.appendChild(card);
         });
-        updateCarouselButtons();
+        carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+        updateCarouselButtons(projects.length, prevButton, nextButton);
     }
 
-    function moveCarousel(direction) {
-        const currentActive = carousel.querySelector('.project-card.active');
-        const nextActive = direction > 0 
-            ? currentActive.nextElementSibling || carousel.firstElementChild
-            : currentActive.previousElementSibling || carousel.lastElementChild;
-    
-        // Efecto de deslizamiento
-        carousel.classList.add('sliding');
-        carousel.style.transform = `translateX(${direction * -100}%)`;
-    
-        setTimeout(() => {
-            carousel.classList.remove('sliding');
-            carousel.style.transform = '';
-    
-            currentActive.classList.remove('active');
-            nextActive.classList.add('active');
-    
-            currentIndex = Array.from(carousel.children).indexOf(nextActive);
-    
-            projects.forEach((_, index) => {
-                const card = carousel.children[index];
-                card.classList.remove('prev', 'next');
-                if (index === (currentIndex - 1 + projects.length) % projects.length) {
-                    card.classList.add('prev');
-                } else if (index === (currentIndex + 1) % projects.length) {
-                    card.classList.add('next');
-                }
-            });
-    
-            updateCarouselButtons();
-        }, 500); // Duración de la transición
+    function moveProjectCarousel(direction) {
+        currentIndex = (currentIndex + direction + projects.length) % projects.length;
+        carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+        updateActiveCard(carousel, currentIndex);
     }
 
-    function updateCarouselButtons() {
-        const shouldHideButtons = projects.length <= 1;
-        prevButton.classList.toggle('hidden', shouldHideButtons);
-        nextButton.classList.toggle('hidden', shouldHideButtons);
-    }
-
-    prevButton.addEventListener('click', () => moveCarousel(-1));
-    nextButton.addEventListener('click', () => moveCarousel(1));
-
-    updateCarousel();
+    prevButton.addEventListener('click', () => moveProjectCarousel(-1));
+    nextButton.addEventListener('click', () => moveProjectCarousel(1));
 
     // Carrusel de certificaciones
     const certifications = [
         {
-            title: "Certificación 1",
-            issuer: "Empresa 1",
-            image: "../assets/certifications/cert1.jpg",
-            verifyLink: "https://example.com/verify1",
-            downloadLink: "../assets/certifications/cert1.pdf"
+            title: "CS50AI: CS50's Introduction to Artificial Intelligence with Python",
+            issuer: "HarvardX",
+            image: "../assets/certifications/GallardoCaballeroJose_CS50AIVerified.pdf",
+            verifyLink: "https://courses.edx.org/certificates/975705ba7ff14d7a8d6718fe3e22a1d6",
+            downloadLink: "../assets/certifications/GallardoCaballeroJose_CS50AIVerified.pdf"
         },
         // Añade más certificaciones aquí
     ];
@@ -152,29 +157,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const certNextButton = document.querySelector('#certificaciones .carousel-button.next');
     let currentCertIndex = 0;
 
-    function createCertificationCard(certification, index) {
+    function createCertificationCard(certification, index, language) {
         const card = document.createElement('div');
         card.className = 'certification-card';
         if (index === currentCertIndex) {
             card.classList.add('active');
-        } else if (index === (currentCertIndex - 1 + certifications.length) % certifications.length) {
-            card.classList.add('prev');
-        } else if (index === (currentCertIndex + 1) % certifications.length) {
-            card.classList.add('next');
         }
         card.innerHTML = `
-            <img src="${certification.image}" alt="${certification.title}" class="certification-image">
+            <div class="certification-image" data-pdf="${certification.image}"></div>
             <div class="certification-info">
                 <h3 class="certification-title">${certification.title}</h3>
                 <p class="certification-issuer">${certification.issuer}</p>
                 <div class="certification-links">
                     <a href="${certification.verifyLink}" class="certification-link" target="_blank" rel="noopener noreferrer">
                         <span class="material-icons">verified</span>
-                        Verificar
+                        ${language === 'es' ? 'Verificar' : 'Verify'}
                     </a>
                     <a href="${certification.downloadLink}" class="certification-link" download>
                         <span class="material-icons">file_download</span>
-                        Descargar PDF
+                        ${language === 'es' ? 'Descargar Certificado' : 'Download Certificate'}
                     </a>
                 </div>
             </div>
@@ -182,58 +183,71 @@ document.addEventListener('DOMContentLoaded', function() {
         return card;
     }
 
-    function updateCertCarousel() {
+    function updateCertCarousel(language) {
         certCarousel.innerHTML = '';
         certifications.forEach((certification, index) => {
-            const card = createCertificationCard(certification, index);
+            const card = createCertificationCard(certification, index, language);
             certCarousel.appendChild(card);
         });
-        updateCertCarouselButtons();
+        certCarousel.style.transform = `translateX(-${currentCertIndex * 100}%)`;
+        updateCarouselButtons(certifications.length, certPrevButton, certNextButton);
+        renderPDFs();
     }
 
     function moveCertCarousel(direction) {
-        const currentActive = certCarousel.querySelector('.certification-card.active');
-        const nextActive = direction > 0 
-            ? currentActive.nextElementSibling || certCarousel.firstElementChild
-            : currentActive.previousElementSibling || certCarousel.lastElementChild;
-    
-        // Efecto de deslizamiento
-        certCarousel.classList.add('sliding');
-        certCarousel.style.transform = `translateX(${direction * -100}%)`;
-    
-        setTimeout(() => {
-            certCarousel.classList.remove('sliding');
-            certCarousel.style.transform = '';
-    
-            currentActive.classList.remove('active');
-            nextActive.classList.add('active');
-    
-            currentCertIndex = Array.from(certCarousel.children).indexOf(nextActive);
-    
-            certifications.forEach((_, index) => {
-                const card = certCarousel.children[index];
-                card.classList.remove('prev', 'next');
-                if (index === (currentCertIndex - 1 + certifications.length) % certifications.length) {
-                    card.classList.add('prev');
-                } else if (index === (currentCertIndex + 1) % certifications.length) {
-                    card.classList.add('next');
-                }
-            });
-    
-            updateCertCarouselButtons();
-        }, 500); // Duración de la transición
-    }
-
-    function updateCertCarouselButtons() {
-        const shouldHideButtons = certifications.length <= 1;
-        certPrevButton.classList.toggle('hidden', shouldHideButtons);
-        certNextButton.classList.toggle('hidden', shouldHideButtons);
+        currentCertIndex = (currentCertIndex + direction + certifications.length) % certifications.length;
+        certCarousel.style.transform = `translateX(-${currentCertIndex * 100}%)`;
+        updateActiveCard(certCarousel, currentCertIndex);
     }
 
     certPrevButton.addEventListener('click', () => moveCertCarousel(-1));
     certNextButton.addEventListener('click', () => moveCertCarousel(1));
 
-    updateCertCarousel();
+    function updateCarouselButtons(totalItems, prevButton, nextButton) {
+        const shouldHideButtons = totalItems <= 1;
+        prevButton.classList.toggle('hidden', shouldHideButtons);
+        nextButton.classList.toggle('hidden', shouldHideButtons);
+    }
+
+    function updateActiveCard(carouselElement, activeIndex) {
+        const cards = carouselElement.querySelectorAll('.project-card, .certification-card');
+        cards.forEach((card, index) => {
+            if (index === activeIndex) {
+                card.classList.add('active');
+            } else {
+                card.classList.remove('active');
+            }
+        });
+    }
+
+    // Render PDFs as images
+    function renderPDFs() {
+        const pdfElements = document.querySelectorAll('[data-pdf]');
+        pdfElements.forEach(element => {
+            const pdfUrl = element.dataset.pdf;
+            pdfjsLib.getDocument(pdfUrl).promise.then(pdf => {
+                pdf.getPage(1).then(page => {
+                    const scale = 0.5; // Reduced scale for smaller image
+                    const viewport = page.getViewport({ scale });
+                    const canvas = document.createElement('canvas');
+                    const context = canvas.getContext('2d');
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+                    const renderContext = {
+                        canvasContext: context,
+                        viewport: viewport
+                    };
+                    page.render(renderContext);
+                    element.appendChild(canvas);
+                    
+                    // Make the canvas responsive
+                    canvas.style.width = '100%';
+                    canvas.style.height = 'auto';
+                    canvas.style.maxWidth = '500px'; // Set a maximum width
+                });
+            });
+        });
+    }
 
     // Internationalization
     const i18next = window.i18next;
@@ -312,6 +326,8 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }, function(err, t) {
             updateContent(i18next.language);
+            updateProjectCarousel(i18next.language);
+            updateCertCarousel(i18next.language);
         });
 
     function updateContent(language) {
@@ -321,5 +337,7 @@ document.addEventListener('DOMContentLoaded', function() {
             element.textContent = i18next.t(key);
         });
         document.documentElement.lang = language;
+        updateProjectCarousel(language);
+        updateCertCarousel(language);
     }
 });
